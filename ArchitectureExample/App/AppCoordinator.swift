08f7e45e6b8a4743887bb92efe85340e2bbf1ed1
r0982749 <r0982749@student.vives.be>
2026-02-02 16:32:12 +0100

    Made the app coordinator an observable object

--- ArchitectureExample/App/AppCoordinator.swift
+++ ArchitectureExample/App/AppCoordinator.swift
@@ -6,11 +6,14 @@
 //
 
 import SwiftUI
+internal import Combine
 
 @MainActor
-final class AppCoordinator {
+final class AppCoordinator: ObservableObject {
+    @Published var name: String = "Tester"
+    
     @ViewBuilder
     func rootView() -> some View {
-        Text("Hello, World!")
+        Text("Hello, \(name)!")
     }
 }
