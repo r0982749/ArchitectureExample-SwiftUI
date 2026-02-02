e0fc514f9471aec1cdbbaeafd7f75ae9ec8c873e
r0982749 <r0982749@student.vives.be>
2026-02-02 14:51:24 +0100

    Initial Commit

---
+++ ArchitectureExample/ArchitectureExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArchitectureExampleApp.swift
+//  ArchitectureExample
+//
+//  Created by Lars Coppieters on 02/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct ArchitectureExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
