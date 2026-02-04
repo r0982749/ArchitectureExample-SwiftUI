a60afa27c8fe2213a8737aa8ee9fd24a3ffe9244
r0982749 <r0982749@student.vives.be>
2026-02-04 10:08:47 +0100

    Added CurrentUserViewModel

---
+++ ArchitectureExample/ViewModels/CurrentUserViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  CurrentUserViewModel.swift
+//  ArchitectureExample
+//
+//  Created by Lars Coppieters on 04/02/2026.
+//
+
+import Foundation
+
+@Observable
+class CurrentUserViewModel {
+    var name: String = "Tester"
+}
