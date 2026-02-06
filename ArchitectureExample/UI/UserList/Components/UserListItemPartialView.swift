7d80b016393068f5d2c69b375027c89d7656c28f
r0982749 <r0982749@student.vives.be>
2026-02-06 10:23:16 +0100

    Added user list item partial view

---
+++ ArchitectureExample/UI/UserList/Components/UserListItemPartialView.swift
@@ -0,0 +1,16 @@
+//
+//  UserListItemPartialView.swift
+//  ArchitectureExample
+//
+//  Created by Lars Coppieters on 06/02/2026.
+//
+
+import SwiftUI
+
+struct UserListItemPartialView: View {
+    var user: User
+    
+    var body: some View {
+        Text("\(user.fullname)")
+    }
+}
