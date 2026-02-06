2eb2978e7d31e506da7645a7b0b4e614b7276fc2
r0982749 <r0982749@student.vives.be>
2026-02-06 10:21:55 +0100

    Added user list view and view model

---
+++ ArchitectureExample/UI/UserList/UserListView.swift
@@ -0,0 +1,16 @@
+//
+//  UserListView.swift
+//  ArchitectureExample
+//
+//  Created by Lars Coppieters on 06/02/2026.
+//
+
+import SwiftUI
+
+struct UserListView: View {
+    @State var userListViewModel = UserListViewModel()
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
---
+++ ArchitectureExample/UI/UserList/UserListViewModel.swift
@@ -0,0 +1,19 @@
+//
+//  UserListViewModel.swift
+//  ArchitectureExample
+//
+//  Created by Lars Coppieters on 06/02/2026.
+//
+
+import Foundation
+
+@Observable
+class UserListViewModel {
+    var users: [User] = []
+    
+    init() {
+        // call to service
+        
+    }
+}
+
